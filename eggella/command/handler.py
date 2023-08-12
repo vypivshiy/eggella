ef492e6428fe6ad43465af3b7dfa8f3aefac3c81
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-08-12 22:07:11 +0300

    rename parser param to tokenizer

--- eggella/command/handler.py
+++ eggella/command/handler.py
@@ -1,12 +1,10 @@
-from typing import Any, Callable, Dict, Optional, Tuple
+from typing import Any, Callable, Dict, List, Optional, Tuple
 
-from eggella.command.abc import (
-    ABCCommandArgumentsCaster,
-    ABCCommandHandler,
-    ABCCommandParser,
-)
+from eggella.command.abc import ABCCommandHandler
 from eggella.command.arg_caster import CommandArgumentsCaster
-from eggella.command.parser import CommandParser, CommandParserRaw
+from eggella.command.parser import TokensParser, TokensParserRaw
+
+ARGS_AND_KWARGS = Tuple[Tuple[Any], Dict[str, Any]]
 
 
 class CommandHandler(ABCCommandHandler):
@@ -14,19 +12,22 @@ class CommandHandler(ABCCommandHandler):
 
     def __init__(
         self,
-        parser: ABCCommandParser = CommandParser(),
-        caster: Optional[ABCCommandArgumentsCaster] = CommandArgumentsCaster(),
+        tokenizer: Callable[[str], List[str]] = TokensParser(),
+        caster: Optional[Callable[[Callable, List[str]], ARGS_AND_KWARGS]] = CommandArgumentsCaster(),
     ):
-        self.parser = parser
+        self.tokenizer = tokenizer
         self.caster = caster
 
     def handle(self, fn: Callable[..., Any], text: str) -> Tuple[Tuple[Any, ...], Dict[str, Any]]:
-        arguments = self.parser(text)
+        arguments = self.tokenizer(text)
         return self.caster(fn, arguments) if self.caster else (tuple(arguments), {})
+
+    def __call__(self, fn: Callable[..., Any], text: str):
+        return self.handle(fn, text)
 
 
 class RawCommandHandler(CommandHandler):
     """disable arguments parse and return raw string"""
 
-    def __init__(self, parser=CommandParserRaw(), caster=None):
-        super().__init__(parser, caster)
+    def __init__(self, tokenizer=TokensParserRaw(), caster=None):
+        super().__init__(tokenizer, caster)
