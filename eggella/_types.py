9df5f3a6d4b44d81c9b73ae9eb868b2d1c4bf455
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-09-06 09:48:23 +0300

    add NoneType

--- eggella/_types.py
+++ eggella/_types.py
@@ -2,6 +2,7 @@ from typing import Any, Callable, Dict, List, Literal, Tuple, Union
 
 from prompt_toolkit.formatted_text import FormattedText
 
+NoneType = type(None)
 ARGS_AND_KWARGS = Tuple[Tuple[Any, ...], Dict[str, Any]]
 CALLABLE_ERR_HANDLER = Callable[[str, BaseException, str, str], Any]
 PromptLikeMsg = Union[str, FormattedText, Callable[..., Union[FormattedText, List[Tuple[str, str]]]]]
